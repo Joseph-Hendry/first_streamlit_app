4bb4ad24d5fdcc3683a573c1ec68b6a34b78b7f3
Joseph Hendry <HendryJ0@protonmail.com>
2023-11-15 13:43:10 +1300

    Add header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
+# Set title
 streamlit.title('My Parents New Healthy Diner')
+
+# Set header
+streamlit.header('Breakfast Menu')
+
+# Set text
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
