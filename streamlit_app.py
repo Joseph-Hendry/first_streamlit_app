6fc59cd1a086d6bcb9fdf91e9543410b8fa1da43
Joseph Hendry <HendryJ0@protonmail.com>
2023-11-15 14:03:55 +1300

    Set picker to the fruit type

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ import pandas
 
 # Read the list
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Display a picker
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
