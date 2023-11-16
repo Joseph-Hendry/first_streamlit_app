bd9b623e260d85e54984815aa97507775c479b19
Joseph Hendry <HendryJ0@protonmail.com>
2023-11-17 11:50:31 +1300

    Make choice box

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,11 +34,15 @@ streamlit.dataframe(fruits_to_show)
 # Fruityvice header
 streamlit.header("Fruityvice Fruit Advice!")
 
+# Add a selector for the fruit
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # Request API
 import requests
 
 # Send GET request
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 
 # Normalize json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
