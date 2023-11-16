64570c24c58a233a280a4f37d45f3e7fd2d74abd
Joseph Hendry <HendryJ0@protonmail.com>
2023-11-17 11:40:27 +1300

    Add requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,8 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 
 # Display table
 streamlit.dataframe(fruits_to_show)
+
+# Request API
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
